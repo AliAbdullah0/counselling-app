ca87f5ec64fa7078d2fddb62334d055df6c0a443
Ali Abdullah <aliabdullah10nov2006@gmail.com>
2026-04-18 13:12:19 +0500

    .

--- app/(patient)/patient/page.tsx
+++ app/(patient)/patient/page.tsx
@@ -1,21 +1,9 @@
-"use client"
+import React from 'react'
 
-import axios from "axios";
-
-const PatientPage = () => {
-  const fetchPatients = async () => {
-    try {
-      const res = await axios("/api/patients");
-      setPatients(res.data);
-    } catch (error) {
-      console.error("Error fetching patients:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+const Patients = () => {
   return (
-    <div>PatientPage</div>
+    <div>Patients</div>
   )
 }
 
-export default PatientPage
+export default Patients
